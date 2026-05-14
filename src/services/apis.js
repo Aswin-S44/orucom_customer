0fc423db5a0122be303d25b4f60681e593922994
Aswin-S-T <aswinswebart15@gmail.com>
2026-05-14 21:56:38 +0530

    new changes

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = `http://10.105.219.171:5000`;
+export const BACKEND_URL = `http://167.71.234.192:5000`;
 
 // export const BACKEND_URL = `https://glamio-web-final-1.onrender.com`;
 export const GOOGLE_SIGNIN_URL = `${BACKEND_URL}/api/v1/auth/signin/google`;
